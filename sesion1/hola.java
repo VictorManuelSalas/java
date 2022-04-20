a1a34812341ab27283e050098cd8c627e03327e7
Victor Manuel Nuñez Salas <98570683+VictorManuelSalas@users.noreply.github.com>
2022-04-19 23:57:49 -0500

    first

---
+++ sesion1/hola.java
@@ -0,0 +1,11 @@
+package sesion1;
+
+public class hola {
+    public static void main(String [] args){
+
+        int hla = 100;
+        System.out.println("Hola LaunchX, este es tu primer proyecto que ejecutas " + hla);
+
+
+    }
+}
